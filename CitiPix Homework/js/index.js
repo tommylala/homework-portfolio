c76ebddc160c5778156312e7872a507b924fd874
tommylala <30609221+tommylala@users.noreply.github.com>
2017-09-03 12:15:44 -0400

    CitiPix Homework

---
+++ CitiPix Homework/js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	
+	$('#submit-btn').click(function(){
+		event.preventDefault();
+		var city = $('#city-type').val();
+		$('#city-type').val('');
+		city = city.toLowerCase().trim();
+		if (city == 'new york city' || city == 'nyc' || city == 'new york') {
+			$('body').attr('class', 'nyc');
+		}
+
+		else if (city == 'sf' || city == 'san francsico' || city =='bay area') {
+			$('body').attr('class', 'sf');
+		}
+
+		else if (city == 'austin' || city =='atx') {
+			$('body').attr('class', 'austin');
+		}
+
+		else if (city =='los angeles' || city == 'la' || city == 'lax') {
+			$('body').attr('class', 'la');
+		}
+
+		else if (city =='sydney' || city == 'syd') {
+			$('body').attr('class', 'sydney');
+		}
+	});
+
+
+
+
+
+
+});
